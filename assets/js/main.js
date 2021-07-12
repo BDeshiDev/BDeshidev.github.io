62f4afcaeed3fbadb6d1ee66c1346656f59bc75a
BDeshiDev <ffalsoy@gmail.com>
2021-07-12 11:40:13 +0600

    border

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 const menuButton = document.querySelector('.menu-btn');
 const hamburger = document.querySelector('.menu-btn__burger');
-const nav = document.querySelector('.nav');
+const nav = document.querySelector('.burger-nav');
 const navMenu = document.querySelector('.menu-nav');
 const navItems = document.querySelectorAll('.menu-nav__item');
 
