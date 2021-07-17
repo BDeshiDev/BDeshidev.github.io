0de4a2bac9b23ee4f7705f7e5f4181f118a6bf92
BDeshiDev <ffalsoy@gmail.com>
2021-07-17 14:26:06 +0600

    template setup

--- _site/assets/js/landingPage.js
+++ _site/assets/js/landingPage.js
@@ -1,6 +1,8 @@
 fetchAndCreateTypeWriters(handleCharTyped);
 let ctaButton = document.querySelector('.cta-button');//only on available now so simple is best
-setTimeout(()=> {
-    ctaButton.classList.add('revealed');
-    console.log('nene')
-}, 5000);//hook into thypewriters later
+if(ctaButton){
+    setTimeout(()=> {
+        ctaButton.classList.add('revealed');
+        console.log('nene')
+    }, 5000);//hook into thypewriters later
+}
