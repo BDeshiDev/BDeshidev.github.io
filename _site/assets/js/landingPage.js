aa2a728b9eff2f14f44cef510c3403813758d9e1
BDeshiDev <ffalsoy@gmail.com>
2021-07-18 21:36:01 +0600

    Removed unwanted files marked in .gitignore

--- _site/assets/js/landingPage.js
+++
@@ -1,9 +0,0 @@
-fetchAndCreateTypeWriters(handleCharTyped);
-let ctaButton = document.querySelector('#games-button');//only on available now so simple is best
-if(ctaButton){
-    ctaButton.classList.add('hide');
-    ctaButton.hidden = false;
-    setTimeout(()=> {
-        ctaButton.classList.remove('hide');
-    }, 5000);//hook into thypewriters later
-}
